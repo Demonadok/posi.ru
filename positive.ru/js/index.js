94199d848849ab45b4cbf0982a35a66bd4fd291b
Maxxeqq <tayrese@mail.ru>
2018-12-20 14:09:01 +0500

    ааа

--- positive.ru/js/index.js
+++
@@ -1,9 +0,0 @@
-$('.toggle').focusin(function() {
-  $(this).addClass('active');
-  $('.search').addClass('move');
-});
-
-$('.toggle').focusout(function() {
-  $(this).removeClass('active').val("");
-  $('.search').removeClass('move');
-});
